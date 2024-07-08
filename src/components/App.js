29a881d726027f242810fb774ed20200b4c24c05
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-08 21:24:11 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
 
+  const ref = useRef()
+
+  const handleClick = ()=>{
+    ref.current.className = 'show'
+  }
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p className="hide" ref={ref} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 }
